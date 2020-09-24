814962d2c89ad0fae8e92ba26325278639df7eb1
Thera1337 <70898199+Thera1337@users.noreply.github.com>
2020-09-24 10:46:50 +0200

    Add project files.

---
+++ TjuvOchPolis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TjuvOchPolis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
